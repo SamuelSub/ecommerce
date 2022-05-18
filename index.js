ae31c8118357ebb2d7fb51b523675c18600f97aa
samuel <samsubas01@gmail.com>
2022-05-18 10:42:23 +0300

    changed port

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-const PORT = 5555 || process.env.PORT;
+const PORT = process.env.PORT || 5555;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
